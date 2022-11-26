ab774bd25b7e639cd172b20e02d6565ca45e99e6
Murari Nayak <rohannayak90@gmail.com>
2022-11-26 19:36:05 +0530

    Build before publish

--- projects/mng-features/src/public-api.ts
+++ projects/mng-features/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/mng-features.service';
 export * from './lib/mng-features.component';
 export * from './lib/mng-features.module';
+
+export * from './lib/components/color-picker/color-picker.component';

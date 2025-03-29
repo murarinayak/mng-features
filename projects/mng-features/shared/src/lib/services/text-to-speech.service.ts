e4de378887bcfa35cf791a047ec028264737b3ef
murarinayak <murari2nayak@gmail.com>
2025-03-29 17:57:01 +0530

    Make couple of fields optional

--- projects/mng-features/shared/src/lib/services/text-to-speech.service.ts
+++ projects/mng-features/shared/src/lib/services/text-to-speech.service.ts
@@ -8,7 +8,7 @@ export class M2NTextToSpeechService {
 
   fns = inject(Functions);
 
-  getAudio(docID: string, text: string, languageCode: string, voiceName: string) {
+  getAudio(docID: string, text: string, languageCode?: string, voiceName?: string) {
     // connectFunctionsEmulator(this.fns, "127.0.0.1", 5001);
     const request: IAudioRequest = { docID, input: { text }, voice: { languageCode, name: voiceName } };
     return from(httpsCallableData<IAudioRequest, string>(this.fns, 'textToSpeech')(request)).pipe(

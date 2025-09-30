c8878a734dae3a140cbf7a7259d9193fa5093814
murarinayak <murari2nayak@gmail.com>
2025-09-30 18:13:56 +0530

    Fix exports for color-picker module

--- projects/mng-features/color-picker/src/public-api.ts
+++ projects/mng-features/color-picker/src/public-api.ts
@@ -1,5 +1,6 @@
 export * from './lib/color-picker.module';
 
 export * from './lib/components/color-picker/color-picker.component';
+export * from './lib/components/color-picker-1/color-picker.component';
 
 export * from './lib/common/color-picker.constants';
